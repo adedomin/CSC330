3f66b8483ccc6ba8947ec5d191bf0daf1df4ebc9
adedomin <anthony@dedominic.pw>
2015-11-20 03:10:39 -0500

    project 4 mostly done

--- project4/TIMER.CPP
+++ project4/TIMER.CPP
@@ -8,7 +8,7 @@ Timer::Timer()
 double Timer::elapsed_time()
 {
    clock_t end_time = clock();
-   return ((double) (end_time - start_time)) / ((double) CLK_TCK);
+   return ((double) (end_time - start_time)) / ((double) CLOCKS_PER_SEC);
 }
  
 void Timer::reset()
